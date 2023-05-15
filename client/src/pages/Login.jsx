a30c9bf93f40d475e2431b4fa1d0eec347540db4
renji18 <aadarshjha1401@gmail.com>
2023-05-15 15:59:11 +0530

    removed server

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Email } from "../components";
 
 const Login = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const submitHandler = () => {
-      navigate("/") 
-     };
-    
+  const submitHandler = () => {
+    navigate("/");
+  };
+
   return (
     <div className="h-screen bg-my-light dark:bg-my-dark flex flex-col items-center ">
-      <Email title="LOGIN" text="NEW USER? REGISTER NOW" linkTo="/register" handleClick={submitHandler} />
+      <Email
+        title="LOGIN"
+        text="NEW USER? REGISTER NOW"
+        linkTo="/register"
+        handleClick={submitHandler}
+      />
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
