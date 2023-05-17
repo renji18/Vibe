7a6ea694be6432035806ed9361f575fc212d9650
renji18 <aadarshjha1401@gmail.com>
2023-05-18 00:29:26 +0530

    merged from staging

--- src/App.js
+++ src/App.js
@@ -5,39 +5,57 @@ import { EnterDetails, Mode } from "./components";
 import { useSelector } from "react-redux";
 
 function App() {
-    const [isDark, setIsDark] = useState(true);
-    const [userTheme, setUserTheme] = useState("dark");
+  const [isDark, setIsDark] = useState(true);
+  const [userTheme, setUserTheme] = useState("dark");
 
-    const {profile} = useSelector(state => state.userData)
+  const { profile } = useSelector((state) => state.userData);
 
-    useEffect(() => {
-      const themeSet = () => {
-        document.documentElement.classList.add("dark");
-        setUserTheme("dark");
-        setIsDark(true);
-        localStorage.setItem("theme", "dark");
-      };
-      themeSet();
-    }, []);
-
-    const themeSwitch = () => {
-      if (document.documentElement.classList.contains("dark")) {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("theme", "light");
-        setUserTheme("light");
-        setIsDark(false);
-        return;
-      }
+  useEffect(() => {
+    const themeSet = () => {
       document.documentElement.classList.add("dark");
       setUserTheme("dark");
       setIsDark(true);
       localStorage.setItem("theme", "dark");
     };
+    themeSet();
+  }, []);
+
+  const themeSwitch = () => {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setUserTheme("light");
+      setIsDark(false);
+      return;
+    }
+    document.documentElement.classList.add("dark");
+    setUserTheme("dark");
+    setIsDark(true);
+    localStorage.setItem("theme", "dark");
+  };
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={!profile ? <Login /> : !profile?.name ? <EnterDetails /> : <Home />} />
-        <Route exact path="/register" element={!profile ? <Register /> : !profile?.name ? <EnterDetails /> : <Home />} />
+        <Route
+          exact
+          path="/"
+          element={
+            !profile ? <Login /> : !profile?.name ? <EnterDetails /> : <Home />
+          }
+        />
+        <Route
+          exact
+          path="/register"
+          element={
+            !profile ? (
+              <Register />
+            ) : !profile?.name ? (
+              <EnterDetails />
+            ) : (
+              <Home />
+            )
+          }
+        />
       </Routes>
       <Mode themeSwitch={themeSwitch} />
     </BrowserRouter>
