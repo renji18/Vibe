854b3ced3b6d4c69597d36a9aed1287afa34e52d
renji18 <aadarshjha1401@gmail.com>
2023-05-17 22:54:15 +0530

    firebase folder structure change

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { FirebaseProvider } from "./firebase/firebase";
+import { FirebaseProvider } from "./firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
